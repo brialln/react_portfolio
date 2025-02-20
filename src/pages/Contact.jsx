6468f8f5d19541f10efa10d68603a437017f77d5
brialln <brianallenlm913@gmail.com>
2025-02-20 14:46:07 +0800

    Modified API for Contact Page | Feb. 20, 2025

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,12 +13,12 @@ const Contact = () => {
         setStatus("Sending message...");
 
         try {
-            const response = await fetch("http://localhost:5000/send-email", {
+            const response = await fetch("https://briallnportfolio.vercel.app/api/send-email", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData),
             });
-
+            
             const data = await response.json();
             setStatus(data.message);
             if (data.success) setFormData({ name: "", email: "", message: "" });
